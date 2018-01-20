968bd8a9fe17ff49a6452b3819ab71f7cc1b6f4b
vedant <vedantlfc@gmail.com>
2018-01-20 23:44:41 +0530

    created permissionssss

--- app/src/main/java/kamal/saqib/spamshield/MainActivity.java
+++ app/src/main/java/kamal/saqib/spamshield/MainActivity.java
@@ -5,12 +5,14 @@ import android.animation.Animator;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.SearchManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -28,6 +30,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
@@ -39,6 +42,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.activeandroid.ActiveAndroid;
@@ -77,16 +81,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private DrawerLayout mDrawer;
     private  android.support.v7.app.ActionBarDrawerToggle toggle;
+    private Menu menu;
 
     private static final int READ_SMS_PERMISSIONS_REQUEST = 1;
     private static final int SEND_SMS_PERMISSIONS_REQUEST = 2;
     private static final int READ_CONTACTS_PERMISSIONS_REQUEST = 3;
     private static final int READ_EXTERNAL_STORAGE_PERMISSION_REQUEST = 4;
     private static final int WRITE_EXTERNAL_STORAGE_PERMISSION_REQUEST = 5;
+    private static final int WRITE_CONTACTS_PERMISSIONS_REQUEST = 6;
 
     private FloatingActionButton fab;
     private RelativeLayout layoutMain;
     private RelativeLayout layoutContent;
+
+    private List<String> items;
+
     private  boolean isOpen=false;
     TextView spamcount;
 
@@ -151,33 +160,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Toast.makeText(getApplicationContext(),"Default Messaging App",Toast.LENGTH_LONG).show();
         }
 
-        //Search Bar
-        MaterialSearchView searchView = (MaterialSearchView) findViewById(R.id.search_view);
-        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //Do some magic
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //Do some magic
-                return false;
-            }
-        });
-
-        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
-            @Override
-            public void onSearchViewShown() {
-                //Do some magic
-            }
-
-            @Override
-            public void onSearchViewClosed() {
-                //Do some magic
-            }
-        });
 
 
         sharedpreferences = getSharedPreferences("Mydata", Context.MODE_PRIVATE);
@@ -535,6 +517,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.navigation_items, menu);
+
+        this.menu = menu;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+
+            SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+            SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
+
+            search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+
+            search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String query) {
+
+                    loadHistory(query);
+
+                    return true;
+
+                }
+
+            });
+
+        }
+
+        return true;
+
+    }
+
+    private void loadHistory(String query) {
+
+        // Cursor
+        String[] columns = new String[] { "_id", "text" };
+        Object[] temp = new Object[] { 0, "default" };
+
+        MatrixCursor cursor = new MatrixCursor(columns);
+
+        for(int i = 0; i < items.size(); i++) {
+
+            temp[0] = i;
+            temp[1] = items.get(i);  //replaced s with i as s not used anywhere.
+
+                    cursor.addRow(temp);
+
+        }
+
+        // SearchView
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        final SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
+
+        search.setSuggestionsAdapter(new SearchAdapter(this, cursor, items));
+
+    }
+
+
+
+
+
+
     public void viewMenu(){
         if(!isOpen){
             int x=layoutContent.getRight();
@@ -629,6 +680,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void  getAllPermission(){
         getPermissionToReadSMS();
         getPermissionToReadContacts();
+        getPermissionToWriteContacts();
         getPermissionToSendSMS();
         getPermissionToReadStorage();
         getPermissionToWriteStorage();
@@ -698,7 +750,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             )){
                 Toast.makeText(this,"Please allow Permission!",Toast.LENGTH_SHORT).show();
             }
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS},
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS},
+                    READ_CONTACTS_PERMISSIONS_REQUEST);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void getPermissionToWriteContacts(){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_CONTACTS)
+                !=PackageManager.PERMISSION_GRANTED){
+            if(shouldShowRequestPermissionRationale(
+                    Manifest.permission.WRITE_CONTACTS
+            )){
+                Toast.makeText(this,"Please allow Permission!",Toast.LENGTH_SHORT).show();
+            }
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS},
                     READ_CONTACTS_PERMISSIONS_REQUEST);
         }
     }
@@ -753,6 +819,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 } else {
                     Toast.makeText(this, "Read Contacts permission denied", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+            break;
+
+            case WRITE_CONTACTS_PERMISSIONS_REQUEST: {
+                if (grantResults.length == 1 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Write Contacts permission granted", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    Toast.makeText(this, "Write Contacts permission denied", Toast.LENGTH_SHORT).show();
                     finish();
                 }
             }
