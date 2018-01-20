afebd7e281a9091e71c10a1aa1dd8e02e546d3bd
vedant <vedantlfc@gmail.com>
2018-01-21 00:05:47 +0530

    created permissionssss

--- app/src/main/java/kamal/saqib/spamshield/MainActivity.java
+++ app/src/main/java/kamal/saqib/spamshield/MainActivity.java
@@ -2,15 +2,18 @@ package kamal.saqib.spamshield;
 
 import android.Manifest;
 import android.animation.Animator;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.SearchManager;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -28,6 +31,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewAnimationUtils;
@@ -39,6 +43,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.activeandroid.ActiveAndroid;
@@ -91,6 +96,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RelativeLayout layoutContent;
     private  boolean isOpen=false;
     TextView spamcount;
+
+    private List<String> items;
+
+    private Menu menu;
 
 
     ListView lv;
@@ -1225,6 +1234,68 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
+
+    }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.navigation_items, menu);
+
+        this.menu = menu;
+
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
+
+        search.setSearchableInfo(manager.getSearchableInfo(getComponentName()));
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+
+                loadHistory(query);
+
+                return true;
+
+            }
+
+        });
+
+        return true;
+
+    }
+
+    // History
+    private void loadHistory(String query) {
+
+        // Cursor
+        String[] columns = new String[] { "_id", "text" };
+        Object[] temp = new Object[] { 0, "default" };
+
+        MatrixCursor cursor = new MatrixCursor(columns);
+
+        for(int i = 0; i < items.size(); i++) {
+
+            temp[0] = i;
+            temp[1] = items.get(i);//replaced s with i as s not used anywhere.
+
+                    cursor.addRow(temp);
+
+        }
+
+        // SearchView
+        SearchManager manager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        final SearchView search = (SearchView) menu.findItem(R.id.search).getActionView();
+
+        search.setSuggestionsAdapter(new SearchAdapter(this, cursor, items));
 
     }
 
